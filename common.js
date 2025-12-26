6cf928867e14e06a8e4be8089fe8f7471b8a6a84
Syam <syamvardhanapu25@gmail.com>
2025-12-26 18:02:55 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function(){
+  						jQuery(".testml-cnt").owlCarousel({
+
+  							loop:true,
+  							margin:20,
+  							autoplay:true,
+  							autoplayTimeout:1000,
+  							autoplayHoverPause:true,
+  							items:2,
+  							autoheight:true,
+  							nav:true,
+  							dots:false
+
+  					});
+					});
+
+
+
+
+ 
